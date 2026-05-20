4c383bb8fd4b2638ff63fc66d4e08e0d5c844e36
axellmuhan <muhamadaxell14@gmail.com>
2026-05-20 09:52:39 +0700

    masukin token env ke supabase

--- services/supabase.ts
+++ services/supabase.ts
@@ -6,8 +6,8 @@ import 'react-native-get-random-values';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL ?? '';
-const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '';
+const SUPABASE_URL = "https://uxybrzmmgxfjxqvdlrfe.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_bw1m2RF6olD35aZyXuSzeA_d4wwUebN";
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   // eslint-disable-next-line no-console
